b40b0a14cd55cdda9ad871f571e149c677750143
broly2605 <107846834+vegetaprince2004@users.noreply.github.com>
2023-12-25 04:13:44 +0530

    .

--- Apna_Dukaan/public/script.js
+++ Apna_Dukaan/public/script.js
@@ -1,10 +1,8 @@
-// public/script.js
 async function signup() {
   const username = document.querySelector('.sign-up-form input[type="text"]').value;
   const email = document.querySelector('.sign-up-form input[type="email"]').value;
   const password = document.querySelector('.sign-up-form input[type="password"]').value;
 
-  // Send the form data to the server
   const response = await fetch('/signup', {
     method: 'POST',
     headers: {
@@ -13,16 +11,31 @@ async function signup() {
     body: JSON.stringify({ username, email, password }),
   });
 
-  // Get the response from the server
-  const result = await response.text();
+  const result = await response.json();
 
-  // Display an alert with the server response
-  alert(result);
+  alert(result.username + ' registered successfully!');
 }
 
-// Add event listeners for switching between sign-in and sign-up modes
+async function login() {
+  const email = document.querySelector('.sign-in-form input[type="email"]').value;
+  const password = document.querySelector('.sign-in-form input[type="password"]').value;
+
+  const response = await fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const result = await response.json();
+
+  alert(result.message);
+}
+
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
+const login_btn = document.querySelector("#login-btn");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
@@ -32,3 +45,5 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+login_btn.addEventListener("click", login);
--- Apna_Dukaan/server.js
+++ Apna_Dukaan/server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -7,7 +6,6 @@ const bcrypt = require('bcrypt');
 const app = express();
 const PORT = 3000;
 
-// Replace 'YOUR_MONGODB_CONNECTION_STRING' with your actual connection string
 mongoose.connect('mongodb+srv://harshbontala188:G1zj60vyPowyjLxr@cluster0.oikyuaw.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -48,17 +46,47 @@ app.post('/signup', async (req, res) => {
       return res.status(400).json({ error: 'Username, email, and password are required' });
     }
 
-    // Hash the password before saving
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email already in use' });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({ username, email, password: hashedPassword });
     const savedUser = await newUser.save();
 
-    // Send the saved user details
     res.json(savedUser.toObject());
   } catch (error) {
     console.error(error);
     res.status(500).send('Error registering user');
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Email and password are required' });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    res.json({ message: 'Login successful', user: user.toObject() });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error during login');
   }
 });
 
